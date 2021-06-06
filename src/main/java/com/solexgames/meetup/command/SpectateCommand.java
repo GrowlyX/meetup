42faf77255a10c5278c52a8eb70dd99a214df7f4
puugz <matej1.svetina@gmail.com>
2021-06-06 19:40:56 +0200

    Now checking if the game has already started before allowing the player to spectate with /spec

--- src/main/java/com/solexgames/meetup/command/SpectateCommand.java
+++ src/main/java/com/solexgames/meetup/command/SpectateCommand.java
@@ -6,6 +6,7 @@ import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Subcommand;
 import com.solexgames.core.util.clickable.Clickable;
 import com.solexgames.meetup.UHCMeetup;
+import com.solexgames.meetup.game.GameState;
 import com.solexgames.meetup.player.GamePlayer;
 import com.solexgames.meetup.util.CC;
 import net.md_5.bungee.api.chat.ClickEvent;
@@ -23,6 +24,11 @@ public class SpectateCommand extends BaseCommand {
 	public void onDefault(Player player) {
 		final GamePlayer gamePlayer = UHCMeetup.getInstance().getPlayerHandler().getByPlayer(player);
 		final boolean spectating = gamePlayer.isSpectating();
+
+		if (UHCMeetup.getInstance().getGameHandler().getGame().isState(GameState.IN_GAME)) {
+			player.sendMessage(CC.RED + "You can't do this anymore.");
+			return;
+		}
 
 		final Clickable clickable = new Clickable("");
 
